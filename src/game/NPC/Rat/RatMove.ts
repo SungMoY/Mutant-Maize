c29f08b780fd267e74fc5747c7d02976b78d32a3
SungMoY <90225118+SungMoY@users.noreply.github.com>
2023-04-23 15:58:04 -0400

    TODO grapple stun, bird AI

--- src/game/NPC/Rat/RatMove.ts
+++ src/game/NPC/Rat/RatMove.ts
@@ -9,46 +9,33 @@ export default class RatWalk extends RatState {
         throw new Error("Method not implemented.");
     }
     public onEnter(options: Record<string, any>): void {
-
+        if (this.parent.velocity.x < 0) {
+            this.owner.animation.playIfNotAlready("RUN_LEFT", true);
+        } else if (this.parent.velocity.x > 0) {
+            this.owner.animation.playIfNotAlready("RUN_RIGHT", true);
+        } else {
+            this.owner.animation.playIfNotAlready("IDLE", false);
+        }
     }
 
     public update(deltaT: number): void {
         super.update(deltaT);
-        // attackbox is in front of its head (rat is facing left)
-        // affected by gravity
-        // starts moving left until velocity is 0
-        // then starts moving right until velocity is 0
-        // check if collision between player and rat's attack box. if so, enter attack state
 
-        // based on velocity, set animation to walk left or walk right
         if (this.parent.velocity.x < 0) {
-            this.owner.animation.play("RUN_LEFT", true);
-        } else if (this.parent.velocity.x > 0) {
-            this.owner.animation.play("RUN_RIGHT", true);
-        } else {
-            this.owner.animation.play("IDLE", true);
-        }
-
-        // check if rat is running into a wall.
-        // if velocity is positive, check pixels to the right of the rat
-        // if velocity is negative, check pixels to the left of the rat
-        // if there is a wall, negate the velocity
-    
-
-        if (this.parent.velocity.x > 0) {
-            // check pixels to the right of the rat
-            if (this.parent.tilemap.getTileAtWorldPosition(new Vec2(this.owner.position.x + this.owner.collisionShape.halfSize.x+1, this.owner.position.y)) !== 0) {
-                this.parent.velocity.x = -this.parent.velocity.x;
-            }
-        } else if (this.parent.velocity.x < 0) {
-            // check pixels to the left of the rat
+            this.owner.animation.playIfNotAlready("RUN_LEFT", true);
             if (this.parent.tilemap.getTileAtWorldPosition(new Vec2(this.owner.position.x - this.owner.collisionShape.halfSize.x-1, this.owner.position.y)) !== 0) {
                 this.parent.velocity.x = -this.parent.velocity.x;
             }
+        } else if (this.parent.velocity.x > 0) {
+            this.owner.animation.playIfNotAlready("RUN_RIGHT", true);
+            if (this.parent.tilemap.getTileAtWorldPosition(new Vec2(this.owner.position.x + this.owner.collisionShape.halfSize.x+1, this.owner.position.y)) !== 0) {
+                this.parent.velocity.x = -this.parent.velocity.x;
+            }
+        } else {
+            this.owner.animation.playIfNotAlready("IDLE", false);
         }
 
         this.owner.move(this.parent.velocity.scaled(deltaT));
-
     }
     
 
